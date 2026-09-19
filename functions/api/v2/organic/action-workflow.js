dc37d564acd2c9eecbafb3baa7e6c66c41cc1eba
sangshacx <133875389+sangshacx@users.noreply.github.com>
2026-09-19 15:35:51 +0800

    Allow AI visibility recovery workflow actions

--- functions/api/v2/organic/action-workflow.js
+++ functions/api/v2/organic/action-workflow.js
@@ -11,7 +11,7 @@ const HEADERS = {
   "Content-Type": "application/json; charset=UTF-8",
 };
 
-const ACTIONS = new Set(["reclaim", "recover", "ctr_opportunity", "optimize", "scale", "protect", "monitor", "review_cannibalization"]);
+const ACTIONS = new Set(["reclaim", "recover", "ctr_opportunity", "optimize", "scale", "protect", "monitor", "review_cannibalization", "ai_visibility_recovery"]);
 const STATUSES = new Set(["new", "in_progress", "done", "snoozed"]);
 
 function json(body, status = 200, headers = {}) {
